0e48fd95b32889da6122ba7f01538f659531d431
Hatem Nassrat <hnassrat@gmail.com>
2016-03-14 10:08:27 -0300

    Updating tech stack slightly + fixing links when compiling tex.

--- resume/distribute.sh
+++ resume/distribute.sh
@@ -4,7 +4,7 @@ public_files=(hatem-nassrat-resume.html hatem-nassrat-resume.pdf hatem-nassrat-r
 private_files=(hatem_nassrat.doc hatem_nassrat.html hatem_nassrat.odt hatem_nassrat.pdf hatem_nassrat.tex)
 
 echo Nassrat.ca
-scp ${private_files[@]} nassrat@nassrat.ca:nassrat.ca/resume/
-scp ${public_files[@]} nassrat@nassrat.ca:nassrat.ca/
+scp ${private_files[@]} nassrat@ssh.nassrat.ca:nassrat.ca/resume/
+scp ${public_files[@]} nassrat@ssh.nassrat.ca:nassrat.ca/
 echo Torch
 scp ${public_files[@]} nassrat@bluenose.cs.dal.ca:public_html/
